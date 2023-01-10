e6bddd12e94c8c62132268b484144ebb6aceaf47
alefra88 <efra.rlz@gmail.com>
2023-01-10 03:25:13 -0600

    agregando apartado literales

---
+++ objLit.js
@@ -0,0 +1,25 @@
+let nombre = "Puki";
+edad = 2;
+
+const perro = {
+  nombre:nombre,
+  edad:edad,
+  ladrar:function(){
+    console.log("guau saca las croquetas prro :B guau")
+  }
+}
+
+console.log(perro)
+perro.ladrar()
+
+const dog = {
+  nombre,
+  edad,
+  raza:"callejas",
+  ladrar(){
+    console.log("guau guau dame de comer prro u.u guau")
+  }
+}
+
+console.log(dog)
+dog.ladrar()
