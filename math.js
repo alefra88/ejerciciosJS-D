194b9c2a036b3f3a91a5c4c6fd682b109a83fae9
alefra88 <efra.rlz@gmail.com>
2023-01-11 21:40:30 -0600

    Algunas funciones del prototipo Math

---
+++ math.js
@@ -0,0 +1,10 @@
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.abs(-7.8));
+console.log(Math.ceil(7.8));
+console.log(Math.floor(7.8));
+console.log(Math.round(7.2));
+console.log(Math.sqrt(81));
+console.log(Math.pow(2, 5));
+console.log(Math.sign(-7.8))//-1 0 1
+console.log(Math.round(Math.random()*1000))
