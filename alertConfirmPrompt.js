b091c6c7968f78152d0362a4af5b6a3b82e0816c
alefra88 <efra.rlz@gmail.com>
2023-01-12 03:31:17 -0600

    conociendo el prototipo window

---
+++ alertConfirmPrompt.js
@@ -0,0 +1,12 @@
+console.log(window);
+// window.alert("Hola esto es una alerta");
+// window.confirm("confirmame plx");
+// window.prompt("rellena acà con algo pss");
+
+// let alerta = window.alert("Hola esto es una alerta");
+// let confirmacion = window.confirm("confirmame plx");
+// let campoRellenar = window.prompt("rellena acà con algo pss");
+
+// console.log(alerta);
+// console.log(confirmacion);
+// console.log(campoRellenar);
