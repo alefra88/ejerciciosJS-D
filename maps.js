2cca4e9e763c677f13702620ec2db731c9abec60
alefra88 <efra.rlz@gmail.com>
2023-01-19 03:30:56 -0600

    terminando de ver que onda con los maps

---
+++ maps.js
@@ -0,0 +1,33 @@
+let mapa = new Map();
+mapa.set("nombre", "Alan");
+mapa.set("apellido", "Ocampo");
+mapa.set("edad", 35);
+
+console.log(mapa);
+console.log(mapa.size);
+console.log(mapa.has("nombre"));
+console.log(mapa.get("nombre"));
+mapa.set("nombre", "Perrito");
+console.log(mapa.get("nombre"));
+mapa.delete("apellido");
+console.log(mapa);
+
+//En los maps podemos generar llaves que no sean cadena de texto, ejem, boolean, null
+
+mapa.set(19, "diecinueve");
+mapa.set(false, "false");
+mapa.set([],{})
+mapa.set(true,{"pulgas":344})
+console.log(mapa)
+
+const mapa2 = new Map([
+  ["nombre","Alan"],
+  ["edad",35],
+  ["Animal","Humano"],
+  ["enojado?","sì, bastante"]
+])
+const keysMapa2 = [...mapa2.keys()]
+const valMapa2 = [...mapa2.values()]
+
+console.log(keysMapa2)
+console.log(valMapa2)
